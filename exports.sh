83859b4ad290e1f92ff4f301013b1461164ebd34
matt <msheckells@gmail.com>
2019-10-18 13:27:38 -0400

    Force python3 on install

--- exports.sh
+++ exports.sh
@@ -3,7 +3,7 @@ export CPP_HOUSEHOLD=$ROBOSCHOOL_PATH/roboschool/cpp-household
 export BULLET_PATH=$HOME/.forked_bullet
 
 if [[ $(uname) == 'Linux' ]]; then
-    PYTHON_BIN=$(readlink -f $(which python))
+    PYTHON_BIN=$(readlink -f $(which python3))
     PYTHON_ROOT=${PYTHON_BIN%/bin/python*}
     PYTHON_VER=${PYTHON_BIN#$PYTHON_ROOT/bin/python}
 fi
@@ -17,6 +17,6 @@ fi
 
 
 export PKG_CONFIG_PATH=${PYTHON_ROOT}/lib/pkgconfig:$BULLET_PATH/lib/pkgconfig/:$PKG_CONFIG_PATH
-export CPATH=$CPATH:${PYTHON_ROOT}/include/python${PYTHON_VER}m:$HOME/.boost/include
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$BULLET_PATH/lib:$HOME/.boost/lib
+export CPATH=${PYTHON_ROOT}/include/python${PYTHON_VER}m:$HOME/.boost/include:$CPATH
+export LD_LIBRARY_PATH=$BULLET_PATH/lib:$HOME/.boost/lib:$LD_LIBRARY_PATH
 
--- install_boost.sh
+++ install_boost.sh
@@ -8,6 +8,6 @@ if [ $(uname) == 'Linux' ]; then
     tar -xf boost_1_69_0.tar.gz
     cd boost_1_69_0
 
-     ./bootstrap.sh --with-python=$(which python) --with-libraries=python --prefix=$HOME/.boost --with-python-root=$PYTHON_ROOT
+     ./bootstrap.sh --with-python=$(which python3) --with-libraries=python --prefix=$HOME/.boost --with-python-root=$PYTHON_ROOT
      ./b2 install > $BOOST_SRCDIR/boost_make.log || (tail -100 $BOOST_SRCDIR/boost_make.log; exit 1)
 fi
